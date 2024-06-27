3eba4dc1c743bb300afb943e14ef35154f130087
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-27 18:03:54 -0400

    agregué algunas funciones en views, actualmente estoy configurando el formulario

--- plantitayika_app/views.py
+++ plantitayika_app/views.py
@@ -1,5 +1,6 @@
 # plantitayika_app/views.py
 from django.shortcuts import render
+from .models import Plantas, Insumos, Maceteros, Clientes
 
 def pagina_inicio(request):
     return render(request, 'plantitayika_app/PaginaInicio.html')
@@ -11,13 +12,30 @@ def pagina_contacto(request):
     return render(request, 'plantitayika_app/PaginaContacto.html')
 
 def pagina_formulario(request):
+    vClientes= Clientes.objects.all()
+    context={"vInsumos":vClientes}
     return render(request, 'plantitayika_app/PaginaFormulario.html')
 
+""" def registrarCliente(request):
+    nombre = request.POST["name"]
+    email = request.POST["name"]
+    contrasena = request.POST["name"]
+    numero = request.POST["name"]
+ """
+
+
 def pagina_insumos(request):
-    return render(request, 'plantitayika_app/PaginaInsumos.html')
+    vInsumos= Insumos.objects.all()
+    context={"vInsumos":vInsumos}
+    return render(request, 'plantitayika_app/PaginaInsumos.html',context)
 
 def pagina_maceteros(request):
-    return render(request, 'plantitayika_app/PaginaMaceteros.html')
+    vMaceteros= Maceteros.objects.all()
+    context={"vInsumos":vMaceteros}
+    return render(request, 'plantitayika_app/PaginaMaceteros.html',context)
 
 def pagina_productos(request):
     return render(request, 'plantitayika_app/PaginaProductos.html')
+
+def index(request):
+    return render(request, 'Alumnos/index.html', context)
