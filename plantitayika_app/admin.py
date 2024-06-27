249d27517516872f555af4425e9ebd7139467ab8
Nicolas Bravo <nicojjj4@gmail.com>
2024-06-27 16:25:44 -0400

    Se agregarón las clases en models, se arregló la url al css, falta agregar imagenes y mostrarlas (productos, etc)

--- plantitayika_app/admin.py
+++ plantitayika_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Plantas, Insumos, Maceteros, Clientes
 # Register your models here.
+admin.site.register(Plantas)
+admin.site.register(Insumos)
+admin.site.register(Maceteros)
+admin.site.register(Clientes)
--- plantitayika_app/models.py
+++ plantitayika_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class Plantas(models.Model):
+    id_planta  = models.AutoField(db_column='idPlanta', primary_key=True) 
+    nombre = models.CharField(max_length=30)
+    precio     = models.IntegerField(blank=False, null=False)
+    descripcion = models.CharField(max_length=500, blank=False, null=False)
+    foto = models.ImageField(upload_to='Plantas/',null=True, blank=True,verbose_name='Foto de la planta')
+
+    def __str__(self):
+        return str(self.Plantas)
+    
+class Maceteros(models.Model):
+    id_Macetero = models.AutoField(db_column='idMacetero', primary_key=True) 
+    nombre = models.CharField(max_length=30)
+    precio     = models.IntegerField(blank=False, null=False)
+    descripcion = models.CharField(max_length=500, blank=False, null=False)
+    foto = models.ImageField(upload_to='Maceteros/',null=True, blank=True,verbose_name='Foto del Macetero')
+
+    def __str__(self):
+        return str(self.Maceteros)
+    
+class Insumos(models.Model):
+    id_Insumo  = models.AutoField(db_column='idInsumo', primary_key=True) 
+    nombre = models.CharField(max_length=30)
+    precio     = models.IntegerField(blank=False, null=False)
+    descripcion = models.CharField(max_length=500, blank=False, null=False)
+    foto = models.ImageField(upload_to='Insumos/',null=True, blank=True,verbose_name='Foto del Insumo')
+
+    def __str__(self):
+        return str(self.Insumos)
+
+class Insumos(models.Model):
+    id_Insumo  = models.AutoField(db_column='idInsumo', primary_key=True) 
+    nombre = models.CharField(max_length=30)
+    precio     = models.IntegerField(blank=False, null=False)
+    descripcion = models.CharField(max_length=500, blank=False, null=False)
+    foto = models.ImageField(upload_to='Insumos/',null=True, blank=True,verbose_name='Foto del Insumo')
+
+    def __str__(self):
+        return str(self.Insumos)
+    
+class Clientes(models.Model):
+    id_Cliente  = models.AutoField(db_column='id_Cliente', primary_key=True) 
+    nombre = models.CharField(max_length=30)
+    email = models.EmailField()
+    contrasena = models.CharField(max_length=30)
+    numero = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.Clientes)
